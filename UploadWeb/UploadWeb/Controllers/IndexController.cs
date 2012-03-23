c92efd2259be2458a58e28844e1031f39e197f56
scoorf <scoorf@orbu.co>
2012-03-23 14:20:38 +0100

    Front-end sort of finished.

---
+++ UploadWeb/UploadWeb/Controllers/IndexController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace UploadWeb.Controllers
+{
+    public class IndexController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
